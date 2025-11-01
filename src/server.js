4c08ec37a4028703eb824c0f6aff90e207fb602a
Vadym Samardak <fatalerror14.88@icloud.com>
2025-11-01 14:51:38 +0100

    added celebrate  middleware error handler to server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 // src/server.js
 import express from 'express';
-import cors from 'cors';
 import 'dotenv/config';
+import cors from 'cors';
+import { errors } from 'celebrate'; // Імпортуємо middleware помилок celebrate
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -35,6 +36,10 @@ app.get('/test-error', (req, res) => {
 
 // підключаємо групу маршрутів нотаток
 app.use(notesRoutes);
+
+// Додаємо middleware помилок celebrate
+app.use(errors());
+// Це має бути розміщено перед іншими обробниками помилок
 
 // 404 і обробник помилок — наприкінці ланцюжка
 app.use(notFoundHandler);
