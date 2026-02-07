ca8e98508e5decab3f479036970ca7297812b811
Vadym Samardak <fatalerror14.88@icloud.com>
2026-02-07 13:58:10 +0100

    dd controllers, validations, utils and routes for mail interactions

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,10 +7,16 @@ import {
   logoutUser,
   registerUser,
   refreshUserSession,
+  requestResetEmail,
+  resetPassword,
+  getResetPasswordPage,
 } from '../controllers/authController.js';
 import {
   loginUserSchema,
   registerUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
+  getResetPasswordPageSchema,
 } from '../validations/authValidation.js';
 
 const router = Router();
@@ -19,5 +25,21 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
+
+router.post(
+  '/auth/reset-password',
+  celebrate(resetPasswordSchema),
+  resetPassword,
+);
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
+router.get(
+  '/auth/reset-password-page',
+  celebrate(getResetPasswordPageSchema),
+  getResetPasswordPage,
+);
 
 export default router;
--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -15,3 +15,25 @@ export const loginUserSchema = {
     password: Joi.string().required(),
   }),
 };
+
+// Схема валідації для запиту на скидання пароля (відправка листа на email)
+export const requestResetEmailSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required(),
+  }),
+};
+
+// Схема валідації для скидання пароля (власне зміна пароля)
+export const resetPasswordSchema = {
+  [Segments.BODY]: Joi.object({
+    password: Joi.string().min(8).required(),
+    token: Joi.string().required(),
+  }),
+};
+
+//схема валідації для отримання сторінки скидання пароля
+export const getResetPasswordPageSchema = {
+  [Segments.QUERY]: Joi.object({
+    token: Joi.string().required(),
+  }),
+};
