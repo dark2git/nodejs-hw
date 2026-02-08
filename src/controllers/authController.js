01ec69a4d0298642e4552ac3c84e1f614c5af4fd
Vadym Samardak <fatalerror14.88@icloud.com>
2026-02-08 17:59:52 +0100

    fix Spelling

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,7 +6,7 @@ import { User } from '../models/user.js';
 import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import jwt from 'jsonwebtoken';
-import { sendEmail } from '../utils/sendMmail.js';
+import { sendEmail } from '../utils/sendMail.js';
 import handlebars from 'handlebars';
 import path from 'node:path';
 import fs from 'node:fs/promises';
